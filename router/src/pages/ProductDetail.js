9f0458d1ebaad5ffe2a332ff2a6e0f7c8a1de4d3
Woong O <woongo@Woongs-MacBook-Pro.local>
2022-07-06 00:29:14 -0400

    router basic

---
+++ router/src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  const params = useParams();
+
+  return (
+    <section>
+      <h1>Product Details</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
+};
+
+export default ProductDetail;
